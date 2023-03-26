de94996025b12f288ca5a23fdff4c6ba4c113098
Maria Fernanda Rios Cardenas <ingfernandarios1214@gmail.com>
2023-03-26 17:19:07 -0500

    Styles module and  global with css.  Control styles with clsx

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,23 +1,28 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from 'next/script';
+import Layout from "../../components/layout";
+import AlertComponent from "../../components/Alert/Alert";
 
 export default function FirstPost() {
-  return <>
-    <Head>
-      <title>Firs post</title>
-      {/* Agregando comandos de terceros */}
-      {/* <Script
+  return (
+    <Layout>
+      <Head>
+        <title>Firs post</title>
+        {/* Agregando comandos de terceros */}
+        {/* <Script
         src="https://connect.facebook.net/en_US/sdk.js"
         strategy="lazyOnload"
         onLoad={() =>
           console.log(`script loaded correctly, window.FB has been populated`)
         }
       /> */}
-    </Head>
-    <h1>First post</h1>
-    <h2>
-      <Link href={"/"}>Back to home</Link>
-    </h2>
-  </>
+      </Head>
+      <h1>First post</h1>
+      <h2>
+        <Link href={"/"}>Back to home</Link>
+      </h2>
+      <AlertComponent children='Mafe'type="success"/>
+      <AlertComponent children='Failed process'type="error"/>
+    </Layout>);
 }
